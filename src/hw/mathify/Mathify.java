5efecbb10d7f3a043dbb8335390c84ef4e6a9193
Veera Venkata Sasanka Uppu <vvsasanka.uppu@gmail.com>
2017-02-16 19:44:05 -0500

    initial commit

---
+++ src/hw/mathify/Mathify.java
@@ -0,0 +1,54 @@
+package hw.pjbk;
+
+import android.app.Application;
+
+public class Mathify extends Application {
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		
+	}
+
+	@Override
+	public void onTerminate()
+	{
+		super.onTerminate();
+		
+	}
+	
+	public String getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(String questionType) {
+		this.questionType = questionType;
+	}
+
+	public int getQuestionsCorrect() {
+		return questionsCorrect;
+	}
+
+	public void setQuestionsCorrect(int questionsCorrect) {
+		this.questionsCorrect = questionsCorrect;
+	}
+	public void incrementQuestionsCorrect() {
+		this.questionsCorrect ++;
+	}
+
+	public int getQuestionsAttempted() {
+		return questionsAttempted;
+	}
+
+	public void setQuestionsAttempted(int questionsAttempted) {
+		this.questionsAttempted = questionsAttempted;
+	}
+	public void incrementQuestionsAttempted() {
+		this.questionsAttempted ++;
+	}
+
+	private String questionType;
+	
+	private int questionsCorrect;
+	private int questionsAttempted;
+}
---
+++ src/hw/mathify/Quiz1Activity.java
@@ -0,0 +1,78 @@
+package hw.pjbk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Quiz1Activity
+	extends Activity
+	implements OnClickListener
+{
+    private Mathify application;
+    
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    public TextView summary;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        application = (Mathify) this.getApplication();
+       button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+       
+        button0 = (Button) findViewById(R.id.button0);
+        button0.setOnClickListener(this);
+        
+        
+    }
+
+    @Override
+    public void onResume()
+    {
+	super.onResume();
+    }
+
+   
+
+    @Override
+    public void onClick(View arg0)
+    {
+	
+	if ( arg0 == button0 ) {
+	    startActivity(new Intent(this, hw.pjbk.NumericQuestion.class));
+	}
+	if ( arg0 == button1 ) {
+	    startActivity(new Intent(this, hw.pjbk.NumericQuestion1.class));
+	}
+	if ( arg0 == button2 ) {
+	    startActivity(new Intent(this, hw.pjbk.NumericQuestion2.class));
+	}
+	if ( arg0 == button3 ) {
+	    startActivity(new Intent(this, hw.pjbk.NumericQuestion3.class));
+	}
+	if ( arg0 == button4 ) {
+	    startActivity(new Intent(this, hw.pjbk.NumericQuestion4.class));
+	}
+	
+    }
+
+}
